eac1ba35ba7ffa08d680ded7d6add38e2f480d15
vedashreeMPatil <vedashreepatil07@gmail.com>
2024-05-09 17:56:17 +0530

    initial init

---
+++ Day_5_Programs/Methods.java
@@ -0,0 +1,22 @@
+public class Methods {
+ 
+    void mul(double num, int num2)
+    {
+        double r=num*num2;
+        System.out.println(r);
+    }
+    void mul(float num1, int num2)
+    {
+        float r1= num1* num2;
+        System.out.println(r1);
+
+
+}
+    public static void main (string args[]) {
+        Methods x=new Methods();
+        x.mul(10.5, 0);
+        Methods x= new Methods();
+        x1.mul(10.5f,)
+    }
+
+}
