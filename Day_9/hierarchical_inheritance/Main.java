77608e3a4338ef07bb13d559a50c549fd7a22080
vedashreeMPatil <vedashreepatil07@gmail.com>
2024-05-13 23:51:50 +0530

    initial commit

---
+++ Day_9/hierarchical_inheritance/Main.java
@@ -0,0 +1,15 @@
+package hierarchical_inheritance;
+
+public class Main {
+
+	public static void main(String[] args) {
+
+		Mumbai m=new Mumbai();
+		Pune p =new Pune();
+		m.capital();
+		p.capital();
+		 
+
+	}
+
+}
